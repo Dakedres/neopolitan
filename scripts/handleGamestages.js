9db2ca33cbc6e6e3aaf65aeb1b7951a68e6f389c
Dakedres <dakedres@debian-BULLSEYE-live-builder-AMD64>
2022-08-12 12:31:33 -0600

    Cleanup scripts

--- scripts/handleGamestages.js
+++ scripts/handleGamestages.js
@@ -1,13 +1,9 @@
-const generateScripts = require('./generateScripts'),
-      compile = require('./util/compile'),
+const compile = require('./util/compile'),
       constants = require('./util/constants')
       path = require('path')
 
 const start = location => {
-  Promise.all([
-    generateScripts(require(location) ),
-    compile(location)
-  ])
+  compile(location)
 
   for(let item in require.cache)
     delete item
